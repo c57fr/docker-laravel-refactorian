923f56cac791a2642ce858cc944bc6318a0aea6a
GrCOTE7 <grcote7@gmail.com>
2025-01-24 11:32:46 +0100

    Vite applied on welcome default view

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: [
+                ".env",
+                ".env.local",
+                "public/assets/**",
+                "resources/css/**",
+                "resources/views/**",
+                "routes/**",
+                "app/**",
+                "lang/**",
+                "config/**",
+            ],
         }),
+        react(),
     ],
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        // chunkSizeWarningLimit: 2000,
+    },
 });
